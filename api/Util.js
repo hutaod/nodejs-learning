4c03c99d0a3418cf94b4cc3e81525ca0fa117539
hutao1 <hutao@akulaku.com>
2020-07-07 18:06:30 +0800

    stream

---
+++ api/Util.js
@@ -0,0 +1,5 @@
+const path = require('path')      // 路径模块
+const qs = require('querystring') // 地址参数解析模块
+const util = require('util')      // 常用工具方法模块
+const url = require('url')        // URL 解析模块
+
---
+++ demo/videotomp3/README.md
@@ -0,0 +1,7 @@
+# 实现一个 MP4 转 MP3 工具
+
+先安装ffmpeg
+
+```bash
+brew install ffmpeg
+```
