6f0a39781e713c4ec849fd73864424aa2448a8cf
hutao1 <hutao@akulaku.com>
2020-07-29 20:03:24 +0800

    koa server

--- demo/koa-server/lib/createServer.js
+++ demo/koa-server/lib/createServer.js
@@ -1,15 +1,11 @@
 'use strict'
 
-const fs = require('fs')
-const url = require('url')
 const path = require('path')
 const Koa = require('koa')
-const Router = require('koa-router')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
 const detectPort = require('./utils/detectPort')
-const mimeType = require('./utils/mimeType')
 const staticMiddle = require('./staticMiddle')
 
 /**
@@ -20,8 +16,6 @@ const staticMiddle = require('./staticMiddle')
 async function createServer({ wwwroot = '', port = 7000 }) {
   // 创建一个Koa实例
   const app = new Koa()
-  // 创建一个路由实例
-  const router = new Router()
   // 创建一个数据库实例，这里用 lowdb 的 JSON 存储来模拟数据库而已
   const adapter = new FileSync(path.resolve(__dirname, '../db.json'))
   const db = low(adapter)
@@ -29,29 +23,23 @@ async function createServer({ wwwroot = '', port = 7000 }) {
   // 初始化数据库，可以看做是数据库的字段定义
   db.defaults({ visits: [], count: 0 }).write()
 
+  // 收集数据
+  app.use(async (ctx, next) => {
+    const done = await next()
+    if (done) {
+      // 数据收集
+    }
+  })
+
+  // 创建静态服务器
   app.use(staticMiddle(wwwroot))
-
-  // router.use('/', async (ctx, next) => {
-  //   const ip = ctx.header['X-Real-IP'] || ''
-  //   const { user, page, action } = ctx.query
-
-  //   // 更新数据库
-  //   db.get('visits').push({ ip, user, page, action }).write()
-  //   db.update('count', n => n + 1).write()
-  //   // 返回更新后的数据库字段
-  //   ctx.body = { success: true, visits: db.get('count') }
-  //   await next()
-  // })
 
   const IP = '127.0.0.1'
   let PORT = await detectPort(port, IP)
   // 把中间件压入队列，等待执行
-  app
-    // .use(router.routes())
-    // .use(router.allowedMethods())
-    .listen(PORT, () => {
-      console.log(`server at http://${IP}:${PORT}`)
-    })
+  app.listen(PORT, () => {
+    console.log(`server at http://${IP}:${PORT}`)
+  })
 }
 
 module.exports = createServer
--- demo/koa-server/lib/staticMiddle.js
+++ demo/koa-server/lib/staticMiddle.js
@@ -1,29 +1,93 @@
-const { resolve } = require('path')
+const path = require('path')
+const fs = require('fs')
+const zlib = require('zlib')
+const mimeType = require('./utils/mimeType')
 
-function static(root, opts = {}) {
-  opts.root = resolve(root)
+/**
+ * 创建静态服务器，拦截资源
+ * @param {string} root 静态服务器目录
+ */
+function static(root) {
   return async function serve(ctx, next) {
     let done = false
 
     if (ctx.method === 'HEAD' || ctx.method === 'GET') {
       try {
-        done = await send(ctx, ctx.path, opts)
+        done = await send(ctx, path.resolve(root))
       } catch (err) {
+        console.log(err)
         if (err.status !== 404) {
           throw err
         }
       }
     }
-
     if (!done) {
       await next()
     }
+    return done
   }
 }
 
 module.exports = static
 
-async function send(ctx, path, opts) {
-  console.log(ctx.path, opts)
+async function send(ctx, root) {
+  let pathname = ctx.path
+  // 获取文件路径
+  const filePath = path.join(root, pathname)
+  // 获取文件后缀
+  const ext = path.extname(pathname)
+
+  // 参数合法性校验
+  // 1. 判断文件是否存在
+  if (!fs.existsSync(filePath)) {
+    throw { status: 404, message: '找不到资源' }
+  }
+
+  // 2. 若文件存在，判断是否是文件类型
+  const fStat = fs.statSync(filePath)
+  if (fStat.isFile()) {
+    // 2.1 文件类型则判断后缀合法性，合法则返回
+    // 非允许后缀的资源不予返回
+    if (!mimeType[ext]) {
+      throw new Error('非允许后缀的资源不予返回')
+    } else {
+      // 若合法存在，判断是否位于 root 目录下
+      if (!filePath.startsWith(root)) {
+        throw new Error(`文件不位于 ${root} 目录下`)
+      }
+
+      // 304 缓存有效期判断, 使用 If-Modified-Since，用 Etag 也可以
+      const modified = ctx.req.headers['if-modified-since']
+      const expectedModified = new Date(fStat.mtime).toGMTString()
+      if (modified && modified == expectedModified) {
+        ctx.status = 304
+        ctx.type = mimeType[ext]
+        ctx.res.setHeader('Cache-Control', 'max-age=3600')
+        ctx.res.setHeader('Last-Modified', expectedModified)
+        return true
+      }
+
+      // 文件头信息设置
+      ctx.status = 200
+      ctx.type = mimeType[ext]
+      ctx.res.setHeader('Cache-Control', 'max-age=3600')
+      ctx.res.setHeader('Content-Encoding', 'gzip')
+      ctx.res.setHeader('Last-Modified', expectedModified)
+
+      // gzip 压缩后，把文件流 pipe 回去
+      const stream = fs.createReadStream(filePath, {
+        flags: 'r',
+      })
+      stream.on('error', () => {
+        ctx.res.statusCode = 404
+        ctx.res.end()
+      })
+      ctx.body = stream.pipe(zlib.createGzip())
+    }
+  } else {
+    // 2.2 文件夹类型则返回文件列表的html页面
+  }
+
+  console.log(root, pathname, filePath, ext)
   return true
 }
